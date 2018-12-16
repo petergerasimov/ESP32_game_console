f02f72dd2113d89c478f1ab6257a449a18c3b244
petergerasimov <petergerasimov2000@gmail.com>
2018-12-16 15:51:53 +0200

    multicore

--- Streaming_client/src/main.cpp
+++ Streaming_client/src/main.cpp
@@ -2,61 +2,59 @@
 #include <EEPROM.h>
 #include <SPI.h>
 #include <math.h>
-
+#include "soc/rtc.h"
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
 using namespace std;
 
+#define MTU 1460
+#define CHUNK_SIZE 512
+
 char host[] = "192.168.96.104";
 int port=1337;
 
+
 WiFiUDP udp;
-uint8_t buffer[1460];
+uint8_t buffer[MTU];
+int buffSize = 0;
 
 void connectToWiFi(char*,char*);
+void loadJPEG(uint8_t*,int);
+void recieveFrame(void*);
 
 void setup(){
+    rtc_clk_cpu_freq_set(RTC_CPU_FREQ_240M);
     GD.begin(0);
-    GD.self_calibrate();
+    // GD.self_calibrate();
     Serial.begin(9600);
     connectToWiFi("Nika","MnogoHubavaParolaZaWIFI");
     udp.begin(port);
+    xTaskCreatePinnedToCore( recieveFrame, "recieverTask",8096, NULL, 1, NULL ,0);
 }
 
 void loop(){
+    double m=millis();
     GD.Clear();
     GD.get_inputs();
-    udp.parsePacket();
+    
 
-    if(udp.read(buffer, 1460) > 0){
+    if(buffSize>0){
         GD.cmd_loadimage(0, 0);
-        int buffSize = 0;
-
-        for(int i=0;i<5;i++){
-            buffSize += buffer[(sizeof(buffer)/sizeof(buffer[0]))-5+i]*pow(10,4-i);
-        }
-
-        int offset = 512;
-        for(int i=0;i<buffSize;i+=offset){
-            offset=min(512, buffSize-i);
-            uint8_t buff_buff[offset];
-            for(int j=0;j<offset;j++){
-                buff_buff[j] = buffer[j+i];
-            }
-            GD.copyram(buff_buff , (offset + 3) & ~3);
-        }
-        
+        loadJPEG(buffer,buffSize);   
         GD.Begin(BITMAPS);
 
         GD.BitmapSize(NEAREST, BORDER, BORDER, 480, 272);
         GD.cmd_scale(F16(4), F16(4));
         GD.cmd_setmatrix();
-
         GD.Vertex2ii(0, 0);
+
+        buffSize = 0;
     }
 
     GD.swap();
+
+    Serial.println("FPS:" + String(1000/(millis()-m)));
 }
 
 void connectToWiFi(char *ssid,char *password){
@@ -81,4 +79,28 @@ void connectToWiFi(char *ssid,char *password){
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+}
+
+void loadJPEG(uint8_t *jpeg,int size){
+    int offset = CHUNK_SIZE;
+    for(int i=0;i<size;i+=offset){
+        offset=min(CHUNK_SIZE, size-i);
+        GD.copyram(&jpeg[i] , (offset + 3) & ~3);
+    }
+}
+
+void recieveFrame(void *p){
+    while(true){
+        if(buffSize==0){
+            udp.parsePacket();
+            if(udp.read(buffer, MTU) > 0){
+                for(int i=0;i<5;i++){
+                    buffSize += buffer[(sizeof(buffer)/sizeof(buffer[0]))-5+i]*pow(10,4-i);
+                }
+            }
+        }
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+    
+
 }
